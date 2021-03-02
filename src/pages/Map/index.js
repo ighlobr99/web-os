2fe6e0faee3b24a2fb8030b723ca784121b53d77
Lucas Gomes <ighlobr@gmail.com>
2021-03-01 22:18:58 -0300

    Inserindo coordenadas

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -53,8 +53,8 @@ const MapData = () => {
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyCxs0m6LclVWmc1cfrePk-dt41lqJzYrzQ' }}
           defaultCenter={{
-            lat: 59.95,
-            lng: 30.33,
+            lat: -24.17,
+            lng: 46.86,
           }}
           defaultZoom={14}
         >
