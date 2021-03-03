098ff06f96747bc41a89a1d5f17e328c6ad9100d
Lucas Gomes <ighlobr@gmail.com>
2021-03-03 19:54:55 -0300

    Corrigindo coordenadas

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -56,7 +56,7 @@ const MapData = () => {
             lat: -23.9991,
             lng: -46.4133,
           }}
-          defaultZoom={1}
+          defaultZoom={5}
         >
           {data && data.length
             ? data.map(mapData => (
