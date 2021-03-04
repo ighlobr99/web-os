4edc8563965a2531795caaa6cfd68bea2ba9ba2f
Lucas Gomes <ighlobr@gmail.com>
2021-03-04 17:19:22 -0300

    Zoom do mapa

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -56,7 +56,7 @@ const MapData = () => {
             lat: -23.9991,
             lng: -46.4133,
           }}
-          defaultZoom={5}
+          defaultZoom={10}
         >
           {data && data.length
             ? data.map(mapData => (
